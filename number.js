6963e26c395def5961aac5a18e612fd36cf94936
huangkai <18778335525@163.com>
2018-12-23 22:04:57 +0800

    math模块扩展

---
+++ number.js
@@ -0,0 +1,12 @@
+// number.isFinite() 用来检查一个数值是否为有限的（ finite）， 即不是Infinity
+Number.isFinite(15);
+Number.isFinite(Infinity);
+
+/**
+ * Math 模块扩展
+ */
+
+// 返回小数的整数部分
+console.log(Math.trunc(4.1));
+
+
